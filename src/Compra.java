b71778177d4dfc10baa332c10e34fb6ad4fcd997
jarvychavez <jav.chavez94@gmail.com>
2025-04-28 21:40:30 -0600

    Tarjeta de credito y compra

---
+++ src/Compra.java
@@ -0,0 +1,25 @@
+public class Compra {
+    //Quiere una descripción y un valor.
+    private double valor;
+    private String descripcion;
+
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: valor = " + valor +
+                ", descripcion='" + descripcion;
+    }
+}
